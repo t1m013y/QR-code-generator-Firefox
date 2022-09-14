69edc29e34d0a792f98cfd01ee9eaa141d9b915d
t1m013y <t1m013y@gmail.com>
2022-09-14 23:12:19 +0300

    Обновление 0.1.2
    Исправлены некоторые ошибки

--- popup/script.js
+++ popup/script.js
@@ -10,12 +10,15 @@ function makeQrCode(data, typeNumber = 0, errorCorrectionLevel = 'M') {
 function updateQrCode(qr, cellSize = 10, margin = cellSize * 0.7) {
 	let placeholder = document.getElementById("placeholder");
 	let qrCodeText = document.getElementById("qrCodeText");
-	let qr_img = qr.qrcode.createImgTag(cellSize, margin);
-	qr_img = stringToElement(qr_img);
-	qr_img.id = "qrCode";
-	qr_img = qr_img.outerHTML;
-	placeholder.innerHTML = qr_img;
-	qrCodeText.innerHTML = qr.data;
+	let qr_img_string = qr.qrcode.createImgTag(cellSize, margin);
+	let qr_img_parser = new DOMParser().parseFromString(qr_img_string, "text/html");
+	let qr_img_parsed_element = qr_img_parser.body.getElementsByTagName("img")[0];
+	let qr_img_src = qr_img_parsed_element.src;
+	let qr_img_element = document.createElement("img");
+	qr_img_element.src = qr_img_src;
+	qr_img_element.id = "qrCode";
+	placeholder.appendChild(qr_img_element);
+	qrCodeText.textContent = qr.data;
 }
 
 function qrCodeError() {
@@ -26,9 +29,9 @@ function qrCodeError() {
 }
 
 let tabs_querying = browser.tabs.query({currentWindow: true, active: true});
-tabs_querying.then(tabs_fulfilled, qrCodeError);
+tabs_querying.then(tabs_querying_fulfilled);
 
-function tabs_fulfilled(tabs) {
+function tabs_querying_fulfilled(tabs) {
 	if (tabs.length != 1) {
 		qrCodeError();
 		return;
@@ -37,10 +40,4 @@ function tabs_fulfilled(tabs) {
 	let currentTabUrl = currentTab.url;
 	let qr = makeQrCode(currentTabUrl);
 	updateQrCode(qr);
-}
-
-function stringToElement(string) {
-	let template = document.createElement('template');
-	template.innerHTML = string;
-	return template.content.firstChild;
 }
